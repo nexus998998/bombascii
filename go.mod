6e8941f0111261f6ae090d09bd2e0ad7e6cf6e2b
orsal <llblblbt@gmail.com>
2026-03-26 09:53:36 -0700

    input detection

---
+++ go.mod
@@ -0,0 +1,9 @@
+module bombascii
+
+go 1.26.1
+
+require (
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+)
--- main.go
+++ main.go
@@ -3,29 +3,58 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
+
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
 )
 
 var width = 20
 var height = 40
+var dirbomboclatX = 0
+var dirbomboclatY = 0
 
 func drawFrame() {
 	frame := ""
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
 			frame += "x"
-
 		}
-		frame += "\n"
+		frame += "\r\n"
 	}
+	fmt.Printf("dirbomboclatX: %d, dirbomboclatY: %d\r\n", dirbomboclatX, dirbomboclatY)
 	fmt.Println(frame)
-
 }
+
 func clearScreen() {
 	fmt.Print("\033[H\033[2J")
 }
 
 func main() {
+
+
 	for {
+		go keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+		switch key.Code {
+		case keys.Up:
+			dirbomboclatX = -1
+			dirbomboclatY = 0
+		case keys.Down:
+			dirbomboclatX = 1
+			dirbomboclatY = 0
+		case keys.Left:
+			dirbomboclatX = 0
+			dirbomboclatY = -1
+		case keys.Right:
+			dirbomboclatX = 0
+			dirbomboclatY = 1
+		case keys.CtrlC:
+			fmt.Print("\033[?25h")
+			os.Exit(0)
+		}
+		return false, nil
+		})
+
 
 		drawFrame()
 		time.Sleep(time.Duration(0.05 * float64(time.Second)))
